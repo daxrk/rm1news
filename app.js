5197724a1efa28c1d4c2f83f64ff1049060a5bd7
Mostafa Mohamed <77597243+daxrk@users.noreply.github.com>
2023-08-24 21:44:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ $(document).ready(function () {
     $("footer span:nth-child(1)").addClass("active1");
     // When the element with the ID 'se' is clicked
     $("#se").click(function () {
-        // Update the source of the embed
-        $("embed").attr("src", "https://www.rm1news.com/search/label/سياسة");
+        // Update the source of the iframe
+        $("iframe").attr("src", "https://www.rm1news.com/search/label/سياسة");
     })
     $("#news").click(function () {
-        // Update the source of the embed
-        $("embed").attr("src", "https://www.rm1news.com/search/label/%D8%A3%D8%AE%D8%A8%D8%A7%D8%B1");
+        // Update the source of the iframe
+        $("iframe").attr("src", "https://www.rm1news.com/search/label/%D8%A3%D8%AE%D8%A8%D8%A7%D8%B1");
     })
     $("#torke").click(function () {
-        // Update the source of the embed
-        $("embed").attr("src", "https://www.rm1news.com/search/label/%D8%AA%D8%B1%D9%83%D9%8A%D8%A7");
+        // Update the source of the iframe
+        $("iframe").attr("src", "https://www.rm1news.com/search/label/%D8%AA%D8%B1%D9%83%D9%8A%D8%A7");
     })
     $("#sport").click(function () {
-        // Update the source of the embed
-        $("embed").attr("src", "https://www.rm1news.com/search/label/%D8%B1%D9%8A%D8%A7%D8%B6%D8%A9");
+        // Update the source of the iframe
+        $("iframe").attr("src", "https://www.rm1news.com/search/label/%D8%B1%D9%8A%D8%A7%D8%B6%D8%A9");
     })
 });
 
